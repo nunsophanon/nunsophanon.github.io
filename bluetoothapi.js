89389d5e621a1975de399eb9fa616e05efff0a70
Nun Sophanon <sophanonnun07@gmail.com>
2020-05-28 09:28:16 +0700

    Update bluetoothapi.js

--- bluetoothapi.js
+++ bluetoothapi.js
@@ -2,7 +2,7 @@ function connectAPI(){
 // １．BLEデバイスをスキャンする
 navigator.bluetooth.requestDevice({
   acceptAllDevices:true, // 全てのデバイスを対象にスキャンを実施する
-  optionalServices:['health_thermometer'] //Health Thermometer
+  //optionalServices:['health_thermometer'] //Health Thermometer
 }).then(device => {
 
   // ２．デバイスに接続
